8c74f55450dc1df5cf21ca639748978470384bc6
narayan-sali <narayanvs015@gmail.com>
2023-12-18 00:21:02 +0530

    small bug fix

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -8,14 +8,14 @@ export const  verifyJWT = asyncHandler(async(req, _, next)=>{
    try {
     const token = req.cookies?.accessToken || req.header
     ("Authorization")?.replace("Bearer ", "")
- 
+      console.log("token", token)
     if(!token){
      throw new ApiError (401, "Unauthorised request")
     }
  
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
  
-    const user = await User.findById(decodedToken?._id).select("-password refreshToken")
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     if (!user){
      throw new ApiError(401, "Invalid  Access Token ")
     }
