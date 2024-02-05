c4498d7d00860b460c6aa0bf48b81193cac71bfb
narayan-sali <narayanvs015@gmail.com>
2024-02-05 18:46:30 +0530

    feat: add toggle video like and unlike

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -6,8 +6,48 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
-    //TODO: toggle like on video
+   
+    if(!(videoId)){
+        throw new ApiError(400, "This video id is not valid")
+    }
+
+    const videoLike = await Like.findOne({
+        video: videoId
+    });
+
+    let like;
+    let unlike;
+
+    if(videoLike){
+        unlike = await Like.deleteOne({
+            video: videoId
+        })
+
+        if(!unlike){
+            throw new ApiError(
+                500,
+                "something went wrong while unlike video !!"
+            )
+        }
+    }else{
+        like = await Like.create({
+            video: videoId,
+            likedBy: req.user._id
+        })
+
+        if(!like){
+            throw new ApiError(
+                500,
+                "something went wrong while like video !!"
+            )
+        }
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, {}, `User ${like? "like": "Unlike"} video successfully !!`)
+    );
 })
+    
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
