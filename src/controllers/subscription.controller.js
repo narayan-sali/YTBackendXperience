7101f7aadd852825e81dcc97ce287c5c55a15849
narayan-sali <narayanvs015@gmail.com>
2024-02-06 03:13:05 +0530

    feat:add  toggle subscription and fixed

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -8,7 +8,43 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
-
+    const userId = req.user._id
+    if(!channelId){
+        throw new ApiError(400, "channel id not found")
+    }
+    
+    const channel = await User.findById({
+        _id: channelId
+    })
+    if(!channel){
+        throw new ApiError(400, "This channel does not Exists")
+    }
+    let subscribe;
+    let unsubscribe;
+    const itHasSubscription = await Subscription.findOne({
+        subscriber: req.user._id,
+        channel: channelId
+    })
+    if(itHasSubscription){
+        unsubscribe = await Subscription.deleteOne({
+            subscriber: req.user._id,
+            channel: channelId
+        })
+        if(!unsubscribe){
+            throw new ApiError(500, "something went wrong while unsubscribe the channel")
+        }
+    }else{
+        subscribe = await Subscription.create({
+            subscriber: req.user._id,
+            channel: channelId
+        })
+        if(!subscribe){
+            throw new ApiError(500, "something went wrong while subscribe the channel")
+        }
+    }
+  return res
+  .status(200)
+  .json(new ApiResponse(200, `User ${subscribe? "subscribe": "unsubscribe"} channel  successfully !!`))
 
 })
 
