67288a6476c379beb8226b62f8fdeade2671eb63
narayan-sali <narayanvs015@gmail.com>
2024-02-07 15:48:27 +0530

    feat : add get , update and  delete tweet

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -26,15 +26,77 @@ const createTweet = asyncHandler(async (req, res) => {
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
+   const  {userId} = req.params
+   
+   if(!userId){
+    throw new ApiError(400, "tweet id not found")
+   }
+   const user = await User.findById(userId)
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+
+    const tweets = await Tweet.aggregate([
+        {
+            $match:{
+                tweetOwner: user._id,
+            }
+            
+        }
+    ]);
+
+   if (!tweets){
+    throw new ApiError("400", " user tweets not found")
+   }
+   return res
+    .status(200)
+    .json(new ApiResponse(200, tweets, "User tweets fetched successfully"))
 })
 
+
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+    const {content}= req.body
+    const {tweetId} = req.params
+    if(!content){
+        throw new ApiError(400, "content not found")
+    }
+    if(!tweetId){
+        throw new ApiError(400, "tweet Id not found")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet){
+        throw new ApiError(400, "tweet not found or invalid tweet Id")
+    }
+    
+    const updatedTweet = await Tweet.findByIdAndUpdate(tweetId,{
+        $set:{
+            content:content,
+            tweetOwner:req.user._id
+        },
+        
+    },
+    {
+        new:true
+    })
+    return res
+    .status(200)
+    .json(new ApiResponse(200, updatedTweet, "User tweets updated successfully"))
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
+    const {tweetId} = req.params
+    if(!tweetId){
+        throw new ApiError(400, "tweet Id not found")
+    }
+    const deletedTweet = await Tweet.findByIdAndDelete(tweetId)
+    if(!deletedTweet){
+        throw new ApiError(400, "unable to delete ")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {},  "User tweets deleted successfully"))
 })
 
 export {
